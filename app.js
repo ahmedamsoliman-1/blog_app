a9ad682212adec950f62b5ddc7cf93527ca8c5ae
Ahmed Ali M. Soliman <40831708+ahmedamsoliman-1@users.noreply.github.com>
2023-05-15 22:02:56 +0400

    update covers

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 // Mongoose/Model config
-// Blog Schema
+// cover Schema
 var blogSchema = new mongoose.Schema({
   title: String,
   image: String,
@@ -34,87 +34,87 @@ var Blog = mongoose.model("Blog", blogSchema);
 
 // RESTful Routes
 app.get("/", function (req, res) {
-  res.redirect("/blogs");
+  res.redirect("/covers");
 });
 
 // Index route
-app.get("/blogs", function (req, res) {
-  Blog.find({}, function (err, blogs) {
+app.get("/covers", function (req, res) {
+  Blog.find({}, function (err, covers) {
     if (err) {
       console.log("Error");
     } else {
-      res.render("index", { blogs: blogs });
+      res.render("index", { covers: covers });
     }
   });
 });
 
 // New route
-app.get("/blogs/new", function (req, res) {
+app.get("/covers/new", function (req, res) {
   res.render("new");
 });
 
 // Create route
-app.post("/blogs", function (req, res) {
-  req.body.blog.body = req.sanitize(req.body.blog.body);
-  //Create blog
-  Blog.create(req.body.blog, function (err, newBlog) {
+app.post("/covers", function (req, res) {
+  req.body.cover.body = req.sanitize(req.body.cover.body);
+  //Create cover
+  Blog.create(req.body.cover, function (err, newcover) {
     if (err) {
       res.render("new");
     } else {
-      res.redirect("/blogs");
+      res.redirect("/covers");
     }
   });
 });
 
 // Show route
-app.get("/blogs/:id", function (req, res) {
-  Blog.findById(req.params.id, function (err, foundBlog) {
+app.get("/covers/:id", function (req, res) {
+  Blog.findById(req.params.id, function (err, foundcover) {
     if (err) {
-      res.redirct("/blogs");
+      res.redirct("/covers");
     } else {
-      res.render("show", { blog: foundBlog });
+      res.render("show", { cover: foundcover });
     }
   });
 });
 
 // Edit route
-app.get("/blogs/:id/edit", function (req, res) {
-  Blog.findById(req.params.id, function (err, foundBlog) {
+app.get("/covers/:id/edit", function (req, res) {
+  Blog.findById(req.params.id, function (err, foundcover) {
     if (err) {
-      res.redirect("/blogs");
+      res.redirect("/covers");
     } else {
-      res.render("edit", { blog: foundBlog });
+      res.render("edit", { cover: foundcover });
     }
   });
 });
 
 // Update route
-app.put("/blogs/:id", function (req, res) {
-  req.body.blog.body = req.sanitize(req.body.blog.body);
+app.put("/covers/:id", function (req, res) {
+  req.body.cover.body = req.sanitize(req.body.cover.body);
   Blog.findByIdAndUpdate(
     req.params.id,
-    req.body.blog,
-    function (err, updatedBlog) {
+    req.body.cover,
+    function (err, updatedcover) {
       if (err) {
-        res.redirect("/blogs");
+        res.redirect("/covers");
       } else {
-        res.redirect("/blogs/" + req.params.id);
+        res.redirect("/covers/" + req.params.id);
       }
     }
   );
 });
 
 // Delete route
-app.delete("/blogs/:id", function (req, res) {
+app.delete("/covers/:id", function (req, res) {
   Blog.findByIdAndUpdate(
     req.params.id,
-    req.body.blog,
-    function (err, updatedBlog) {
-      Blog.findByIdAndRemove(req.params.id, function (err) {
+    req.body.cover,
+    function (err, updatedcover) {
+      cover.findByIdAndRemove(req.params.id, function (err) {
         if (err) {
-          res.redirect("/blogs");
+          res.redirect("/covers");
         } else {
-          res.redirect("/blogs");
+          res.redirect("/covers");
         }
       });
     }
